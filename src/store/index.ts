8b7f07cb9b35e68770cfbafb915acb4d4de78a22
ThibaultMINNEBOO <thibault.minneboo@etudiant.univ-reims.fr>
2024-12-09 18:39:00 +0100

    feat: add profile slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import profileReducer from "./profiles/profileSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    profile: profileReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
---
+++ src/store/profiles/profileSlice.ts
@@ -0,0 +1,55 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface ProfileState {
+  id: number;
+  username: string;
+  description: string;
+  photo: string;
+}
+
+export interface ProfilePayload {
+  username: string;
+  description: string;
+  photo: string;
+}
+
+const initialState: ProfileState[] = [
+  {
+    id: 1,
+    username: "John Doe",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    photo: "https://avatar.iran.liara.run/public",
+  },
+];
+
+export const profileSlice = createSlice({
+  name: "profile",
+  initialState,
+  reducers: {
+    addProfile: (state, action: PayloadAction<ProfilePayload>) => {
+      state.push({
+        id: state.length + 1,
+        ...action.payload,
+      });
+    },
+    removeProfile: (state, action: PayloadAction<number>) => {
+      return state.filter((profile) => profile.id !== action.payload);
+    },
+    updateProfile: (state, action: PayloadAction<ProfileState>) => {
+      return state.map((profile) => {
+        if (profile.id === action.payload.id) {
+          return {
+            ...profile,
+            ...action.payload,
+          };
+        }
+
+        return profile;
+      });
+    },
+  },
+});
+
+export const { addProfile, removeProfile, updateProfile } =
+  profileSlice.actions;
+export default profileSlice.reducer;
